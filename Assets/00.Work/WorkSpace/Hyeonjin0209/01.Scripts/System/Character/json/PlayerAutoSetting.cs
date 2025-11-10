22ba7c569686f767e468899c39d5e79e69c5d55c
hyeonjin0209 <jonmoshyeonmin@gmail.com>
2025-11-10 12:20:04 +0900

    feat : null

--- Assets/00.Work/WorkSpace/Hyeonjin0209/01.Scripts/System/Character/json/PlayerAutoSetting.cs
+++ Assets/00.Work/WorkSpace/Hyeonjin0209/01.Scripts/System/Character/json/PlayerAutoSetting.cs
@@ -14,33 +14,33 @@ public struct CharacterPrefabEntry
 
 public class PlayerAutoSetting : MonoBehaviour
 {
-    [SerializeField] private RectTransform[] settingIconUI;
-    [SerializeField] private Transform[] SpawnPoints;
-    [SerializeField] private List<CharacterPrefabEntry> prefabEntries;
+    [SerializeField] private RectTransform[] settingIconUI;//드래그 앤 드랍 용 캐릭터 UI 스폰 위치
+    [SerializeField] private Transform[] SpawnPoints;//캐릭터 스폰 위치
+    [SerializeField] private List<CharacterPrefabEntry> prefabEntries; // 딕셔너리
 
-    private Dictionary<CharacterType, GameObject> prefabMap;
-    private Dictionary<CharacterType, Sprite> spriteMap;
-    private SaveData partyData;
+    private Dictionary<CharacterType, GameObject> prefab;//프리팹 사전
+    private Dictionary<CharacterType, Sprite> sprite;//스프라이트  사전
+    private SaveData partyData; // 파티 데이터
 
     private void Start()
     {
-        prefabMap = new Dictionary<CharacterType, GameObject>();
-        spriteMap = new Dictionary<CharacterType, Sprite>();
+        prefab = new Dictionary<CharacterType, GameObject>(); // 초기화
+        sprite = new Dictionary<CharacterType, Sprite>();// 초기화
         foreach (var e in prefabEntries)
-            prefabMap[e.type] = e.prefab;
+            prefab[e.type] = e.prefab;//타입 담기(선택한 캐릭터에 따른 픠팹을 생성하기 위해서
         foreach (var o in prefabEntries)
-            spriteMap[o.type] = o.icon;
+            sprite[o.type] = o.icon;//타입 담기(선택한 캐릭터에 따른 스프라이트를 집어넣기 위해서
 
         // 저장된 데이터 불러오기
         partyData = PartyInfo.Load<SaveData>("party_002");
-//==========================================================================================//
-        var c = partyData.party; // List<CharacterType>
+        //==========================================================================================//
+        var c = partyData.party; //c에다 파티 데이터 담기
 
         for (int i = 0; i < c.Count; i++)
         {
             var type = c[i];                        // 복제 할 녀석(이미 party에 담겨져 있음)
             var parent = settingIconUI[i].transform;   // 슬롯 위치
-            var prefab = prefabMap[type];           // 해당 타입의 프리팹 가져오기
+            var prefab = this.prefab[type];           // 해당 타입의 프리팹 가져오기
 
             var clone = Instantiate(prefab, parent, false);
 
@@ -54,13 +54,13 @@ public class PlayerAutoSetting : MonoBehaviour
             Destroy(clone.GetComponent<CharacterSelect>());
             clone.GetComponent<Image>().color = Color.white;
         }
-
+//=============================================================================================//
         for (int i = 0; i < c.Count; i++)
         {
             var type = c[i];                        // 복제 할 녀석(이미 party에 담겨져 있음)
             var parent = SpawnPoints[i].transform;   // 슬롯 위치
-            var prefab = prefabMap[type];           // 해당 타입의 프리팹 가져오기
-            var sprite = spriteMap[type];
+            var prefab = this.prefab[type];           // 해당 타입의 프리팹 가져오기
+            var sprite = this.sprite[type];
             var character = Instantiate(prefab, parent, false);
 
             transform.position = parent.transform.position;
@@ -72,6 +72,9 @@ public class PlayerAutoSetting : MonoBehaviour
             Destroy(character.GetComponent<CanvasRenderer>());
             var sr = character.AddComponent<SpriteRenderer>();
             sr.sprite = sprite;
+            if (sr == null) Debug.Log("1");
+            if (sr.sprite == null) Debug.Log("2");
+            if (sprite == null) Debug.Log("3");
         }
     }
 }
